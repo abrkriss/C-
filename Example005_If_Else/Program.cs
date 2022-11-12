35c79b8fcde00ec411dbdb9d4a8ec0c1190a1ca2
Kristina <abrkriss@gmail.com>
2022-11-12 20:05:52 +0300

    Example005_If_Else

---
+++ Example005_If_Else/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите имя пользователя ");
+        string username = Console.ReadLine();
+
+        if (username.ToLower() == "маша") 
+        {
+            Console.WriteLine("Ура, это же МАША!");
+        }
+
+         else
+        {
+            Console.Write("Привет, ");
+            Console.WriteLine(username);
+        }
+    }
+}
